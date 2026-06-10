cd85bfa6a7910c299cec9d405742ef5028ae5ffa
Mandeep <mandeep.singh.build@gmail.com>
2026-06-10 21:51:41 +0530

    Add initial structure for Utility Library in Python
    This commit introduces a new Python file for the Utility Library, outlining the steps for building a production-ready feature. The file serves as a foundational template, setting the stage for future development and implementation of various utility functions.

---
+++ 5. Mini_Project — Utility _Library (utils.py)/utility_library.py
@@ -0,0 +1,22 @@
+# Build Mini Project — Utility Library (utils.py) — A real-world simulation of building 
+  # a production-ready feature by combining multiple concepts from fucntion part.
+
+
+
+# Step 1:
+
+
+
+# Step 2:
+
+
+
+# Step 3:
+
+
+
+# Step 4:
+
+
+
+# Step 5:
